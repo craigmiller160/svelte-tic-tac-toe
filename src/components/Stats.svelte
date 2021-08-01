a38fff575d09172ab370dea26049f2b81aea9914
Craig Miller <craigmiller160@gmail.com>
2021-08-01 15:00:24 -0400

    Added Current Turn to stats

--- src/components/Stats.svelte
+++ src/components/Stats.svelte
@@ -1,8 +1,32 @@
 
-<style lang="scss">
+<script lang="ts">
 
+    import {game} from '../stores';
+    import type {Game} from '../types/Game';
+
+    let currentTurnValue: string;
+    game.subscribe((gameValue: Game) => {
+        currentTurnValue = gameValue.currentTurn;
+    });
+
+</script>
+
+<style lang="scss">
+  .stats {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    p {
+      font-size: 1.17em;
+    }
+  }
 </style>
 
 <div class="stats">
     <h2>Stats</h2>
+    <p>
+        <strong>Current Turn: </strong>
+        <span>{currentTurnValue}</span>
+    </p>
 </div>
