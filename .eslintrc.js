d4a2a4f2a52e6353075a5ff8b9697210a62f424c
Craig Miller <craigmiller160@gmail.com>
2021-08-01 20:42:59 -0400

    Added eslint

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+    parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module'
+    },
+    env: {
+        es6: true,
+        browser: true
+    },
+    plugins: [
+        'svelte3'
+    ],
+    overrides: [
+        {
+            files: [
+                '*.svelte'
+            ],
+            processor: 'svelte3/svlete3'
+        }
+    ]
+};
