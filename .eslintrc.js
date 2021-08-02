42e28a21e3f8b9ed010bdeef0b4971c116f5539f
Craig Miller <craigmiller160@gmail.com>
2021-08-01 20:48:20 -0400

    Committing all progress

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,10 @@
 const path = require('path');
 
 module.exports = {
+    root: true,
+    extends: [
+        'eslint:recommended'
+    ],
     parserOptions: {
         ecmaVersion: 2021,
         sourceType: 'module'
