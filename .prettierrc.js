f365aa2e4af4d66114a9c75bd986d987b6605fb6
Craig Miller <craigmiller160@gmail.com>
2021-08-03 12:33:51 -0400

    Adding prettier

---
+++ .prettierrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+    arrowParens: 'always',
+    singleQuote: true,
+    printWidth: 90,
+    plugins: ['prettier-plugin-svelte'],
+    semi: true,
+    svelteSortOrder: 'options-styles-scripts-markup',
+    svelteStrictMode: false,
+    svelteBracketNewLine: true,
+    svelteIndentScriptAndStyle: true,
+    trailingComma: 'none',
+    tabWidth: 4,
+    useTabs: true,
+    jsxBracketSameLine: false,
+    quoteProps: 'as-needed',
+    bracketSpacing: true,
+    endOfLine: 'lf'
+}
